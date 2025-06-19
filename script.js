ad3bed0ff149554b6c702ff1ad2f1ccc3f13a4e6
Avinash Kumar <165828196+avinasha6@users.noreply.github.com>
2025-06-19 15:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const boxes = document.querySelectorAll('.image');
+  let draggedBox = null;
+
+  boxes.forEach(box => {
+    image.addEventListener('dragstart', () => {
+      draggedBox = image;
+    });
+
+    image.addEventListener('dragover', e => {
+      e.preventDefault(); // Necessary to allow drop
+    });
+
+    image.addEventListener('drop', () => {
+      if (draggedBox && draggedBox !== image) {
+        // Swap the image sources
+        const draggedImg = draggedBox.querySelector('.image');
+        const targetImg = box.querySelector('.image');
+
+        const tempSrc = draggedImg.src;
+        draggedImg.src = targetImg.src;
+        targetImg.src = tempSrc;
+      }
+    });
+  });
